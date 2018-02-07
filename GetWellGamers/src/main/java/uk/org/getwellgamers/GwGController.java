75bb13f989a98b627fbe6c0da6e6e9ad21f959a2
PETTSP2 <PETTSP2@tdukpc15532.tdwh.co.uk>
2018-02-07 06:57:16 +0000

    pre-TMA01

--- GetWellGamers/src/main/java/uk/org/getwellgamers/GwGController.java
+++ GetWellGamers/src/main/java/uk/org/getwellgamers/GwGController.java
@@ -8,19 +8,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("TODO")
 class GwGController{
 
 	@RequestMapping(method = RequestMethod.GET)
 	  public String root(Locale locale, ModelMap model) {
-		   model.addAttribute("content", "helloWorldView");     
-		   return "index.html";
+		   model.addAttribute("content", "GwGIndex");     
+		  // return "index";
+		   return   "redirect:index.html";
+		 
 	}
-//	ModelAndView
-//	index()
-//	{
-//		ModelAndView mav = new ModelAndView("index");
-//		mav.addObject("version", "0.1");
-//		return mav;
-//	}
 }
