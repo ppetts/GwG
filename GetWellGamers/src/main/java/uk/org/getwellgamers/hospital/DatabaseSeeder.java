410198cceafb4eb704ca96e5f5c3fcc3daf64f15
PETTSP2 <PETTSP2@tdukpc15532.tdwh.co.uk>
2018-03-13 20:06:38 +0000

    Hospitals

--- GetWellGamers/src/main/java/uk/org/getwellgamers/hospital/DatabaseSeeder.java
+++ GetWellGamers/src/main/java/uk/org/getwellgamers/hospital/DatabaseSeeder.java
@@ -1,5 +1,7 @@
 package uk.org.getwellgamers.hospital;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class DatabaseSeeder implements CommandLineRunner {
 	
+	//Generate the Hospital Data for the Database 
 	private HospitalRepositry hospitalResp;
 	
 	@Autowired
@@ -18,15 +21,20 @@ public class DatabaseSeeder implements CommandLineRunner {
 	
 	@Override
 	public void run(String... arg0) throws Exception {
+		
+		ArrayList<Hospital> Hospitals = new ArrayList<Hospital>();
+		
 		Hospital hosp = new Hospital();
 		hosp.setName("Ormskirk");
 		hosp.setAddress1("TODO");
+		Hospitals.add(hosp);
 		
-		hospitalResp.save(hosp);
 		
-		
-		hosp.setName("Addenbrooke's Hospital");
-		hospitalResp.save(hosp);
+		Hospital hosp2 = new Hospital();
+		hosp2.setName("Addenbrooke's Hospital");
+		Hospitals.add(hosp2);
+	
+		hospitalResp.save(Hospitals);
 		
 	}
 
