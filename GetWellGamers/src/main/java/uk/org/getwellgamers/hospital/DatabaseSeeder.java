0bcf96a817fe5f8d61989e7773755759abd09a89
PETTSP2 <PETTSP2@tdukpc15532.tdwh.co.uk>
2018-03-20 09:03:39 +0000

    Hospital seeder

--- GetWellGamers/src/main/java/uk/org/getwellgamers/hospital/DatabaseSeeder.java
+++ GetWellGamers/src/main/java/uk/org/getwellgamers/hospital/DatabaseSeeder.java
@@ -1,6 +1,8 @@
 package uk.org.getwellgamers.hospital;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -10,32 +12,62 @@ import org.springframework.stereotype.Component;
 @Order(1)
 @Component
 public class DatabaseSeeder implements CommandLineRunner {
-	
+
 	//Generate the Hospital Data for the Database 
 	private HospitalRepositry hospitalResp;
-	
+
 	@Autowired
 	public DatabaseSeeder(HospitalRepositry hospitalResp) {
 		this.hospitalResp = hospitalResp;
 	}
-	
+
 	@Override
 	public void run(String... arg0) throws Exception {
-		
+
 		ArrayList<Hospital> Hospitals = new ArrayList<Hospital>();
-		
-		Hospital hosp = new Hospital();
-		hosp.setName("Ormskirk");
-		hosp.setAddress1("TODO");
-		Hospitals.add(hosp);
-		
-		
-		Hospital hosp2 = new Hospital();
-		hosp2.setName("Addenbrooke's Hospital");
-		Hospitals.add(hosp2);
-	
+
+		//TODO Read the CSV from gov website
+
+		//Read this dynamically?  What if the site is down?
+		//http://media.nhschoices.nhs.uk/data/foi/Hospital.csv
+		//CSV might be saftest, can always redonload if required.  Fairly static data, & we have the option to manipulate
+		Scanner scanner = new Scanner(new File("C:\\Users\\pettsp2\\Desktop\\TM470\\HospitalTab.csv"));
+		//  scanner.useDelimiter("\t");
+		while(scanner.hasNextLine()){
+
+			String line = scanner.nextLine();
+			String[] hospDetails = line.split("\t");
+
+			System.out.println("Adding : " + hospDetails[7]);
+			
+			Hospital hosp = new Hospital();
+			hosp.setOrganisationID(hospDetails[0]);
+			hosp.setOrganisationCode(hospDetails[1]);
+			hosp.setOrganisationType(hospDetails[2]);
+			hosp.setSubType(hospDetails[3]);
+			hosp.setSector(hospDetails[4]);
+			hosp.setOrganisationStatus(hospDetails[5]);
+			hosp.setIsPimsManaged(Boolean.getBoolean(hospDetails[6]));
+			hosp.setOrganisationName(hospDetails[7]);
+			hosp.setAddress1(hospDetails[8]);
+			hosp.setAddress2(hospDetails[9]);
+			hosp.setAddress3(hospDetails[10]);
+			hosp.setCity(hospDetails[11]);
+			hosp.setCounty(hospDetails[12]);
+			hosp.setPostcode(hospDetails[13]);
+//			hosp.setLatitude(Long.parseLong(hospDetails[14]));
+//			hosp.setLongitude(Long.parseLong(hospDetails[15]));
+			hosp.setParentODSCode(hospDetails[16]);
+			hosp.setParentName(hospDetails[17]);
+	//		hosp.setPhone(hospDetails[18]);
+	//		hosp.setEmail(hospDetails[19]);
+	//		hosp.setWebsite(hospDetails[20]);
+	//		hosp.setFax(hospDetails[21]);
+			Hospitals.add(hosp);		   
+		}
+		scanner.close();
 		hospitalResp.save(Hospitals);
-		
+
 	}
 
 }
--- GetWellGamers/src/main/java/uk/org/getwellgamers/hospital/Hospital.java
+++ GetWellGamers/src/main/java/uk/org/getwellgamers/hospital/Hospital.java
@@ -5,111 +5,208 @@ import javax.persistence.*;
 @Entity
 public class Hospital {
 	
-	
-	//TODO - Auto generate id
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
-	private long hospitalId;
-	String name;
-	String address1;
-	String address2;
-	String address3;
-	String town;
-	String county;
-	String postcode;
-	String telephone;
+	private String OrganisationID;
+	private String OrganisationCode;
+	private String OrganisationType;
+	private String SubType;
+	private String Sector;
+	private String OrganisationStatus;	
+	private boolean IsPimsManaged;
+	private String OrganisationName;
+	private String Address1;	
+	private String Address2;	
+	private String Address3;	
+	private String City;
+	private String County;	
+	private String Postcode;
+	private long Latitude;
+	private long Longitude;
+	private String ParentODSCode;
+	private String ParentName;
+	private String Phone;
+	private String Email;
+	private String Website;
+	private String Fax;
 	
 	public Hospital() {
 		super();
 	}
 
-	
-	public long getHospitalId() {
-		return hospitalId;
+	public String getOrganisationID() {
+		return OrganisationID;
 	}
 
-
-	public void setHospitalId(long id) {
-		this.hospitalId = id;
+	public void setOrganisationID(String organisationID) {
+		OrganisationID = organisationID;
 	}
 
-
-	public String getName() {
-		return name;
+	public String getOrganisationCode() {
+		return OrganisationCode;
 	}
 
-
-	public void setName(String name) {
-		this.name = name;
+	public void setOrganisationCode(String organisationCode) {
+		OrganisationCode = organisationCode;
 	}
 
+	public String getOrganisationType() {
+		return OrganisationType;
+	}
+
+	public void setOrganisationType(String organisationType) {
+		OrganisationType = organisationType;
+	}
+
+	public String getSubType() {
+		return SubType;
+	}
+
+	public void setSubType(String subType) {
+		SubType = subType;
+	}
+
+	public String getSector() {
+		return Sector;
+	}
+
+	public void setSector(String sector) {
+		Sector = sector;
+	}
+
+	public String getOrganisationStatus() {
+		return OrganisationStatus;
+	}
+
+	public void setOrganisationStatus(String organisationStatus) {
+		OrganisationStatus = organisationStatus;
+	}
+
+	public boolean isIsPimsManaged() {
+		return IsPimsManaged;
+	}
+
+	public void setIsPimsManaged(boolean isPimsManaged) {
+		IsPimsManaged = isPimsManaged;
+	}
+
+	public String getOrganisationName() {
+		return OrganisationName;
+	}
+
+	public void setOrganisationName(String organisationName) {
+		OrganisationName = organisationName;
+	}
 
 	public String getAddress1() {
-		return address1;
+		return Address1;
 	}
-
 
 	public void setAddress1(String address1) {
-		this.address1 = address1;
+		Address1 = address1;
 	}
-
 
 	public String getAddress2() {
-		return address2;
+		return Address2;
 	}
-
 
 	public void setAddress2(String address2) {
-		this.address2 = address2;
+		Address2 = address2;
 	}
-
 
 	public String getAddress3() {
-		return address3;
+		return Address3;
 	}
-
 
 	public void setAddress3(String address3) {
-		this.address3 = address3;
+		Address3 = address3;
 	}
 
-
-	public String getTown() {
-		return town;
+	public String getCity() {
+		return City;
 	}
 
+	public void setCity(String city) {
+		City = city;
+	}
 
 	public String getCounty() {
-		return county;
+		return County;
 	}
-
 
 	public void setCounty(String county) {
-		this.county = county;
+		County = county;
 	}
-
-
-	public void setTown(String town) {
-		this.town = town;
-	}
-
 
 	public String getPostcode() {
-		return postcode;
+		return Postcode;
 	}
-
 
 	public void setPostcode(String postcode) {
-		this.postcode = postcode;
+		Postcode = postcode;
 	}
 
-
-	public String getTelephone() {
-		return telephone;
+	public long getLatitude() {
+		return Latitude;
 	}
 
-
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
+	public void setLatitude(long latitude) {
+		Latitude = latitude;
 	}
+
+	public long getLongitude() {
+		return Longitude;
+	}
+
+	public void setLongitude(long longitude) {
+		Longitude = longitude;
+	}
+
+	public String getParentODSCode() {
+		return ParentODSCode;
+	}
+
+	public void setParentODSCode(String parentODSCode) {
+		ParentODSCode = parentODSCode;
+	}
+
+	public String getParentName() {
+		return ParentName;
+	}
+
+	public void setParentName(String parentName) {
+		ParentName = parentName;
+	}
+
+	public String getPhone() {
+		return Phone;
+	}
+
+	public void setPhone(String phone) {
+		Phone = phone;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+	public String getWebsite() {
+		return Website;
+	}
+
+	public void setWebsite(String website) {
+		Website = website;
+	}
+
+	public String getFax() {
+		return Fax;
+	}
+
+	public void setFax(String fax) {
+		Fax = fax;
+	}
+
 }
