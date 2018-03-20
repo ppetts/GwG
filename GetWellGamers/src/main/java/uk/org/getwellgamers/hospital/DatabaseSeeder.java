b45941034ff403a4777a1537b2e3cc9970efe71c
PETTSP2 <PETTSP2@tdukpc15532.tdwh.co.uk>
2018-03-20 10:46:40 +0000

    Seeding

--- GetWellGamers/src/main/java/uk/org/getwellgamers/hospital/DatabaseSeeder.java
+++ GetWellGamers/src/main/java/uk/org/getwellgamers/hospital/DatabaseSeeder.java
@@ -38,36 +38,38 @@ public class DatabaseSeeder implements CommandLineRunner {
 			String line = scanner.nextLine();
 			String[] hospDetails = line.split("\t");
 
-			System.out.println("Adding : " + hospDetails[7]);
-			
 			Hospital hosp = new Hospital();
-			hosp.setOrganisationID(hospDetails[0]);
-			hosp.setOrganisationCode(hospDetails[1]);
-			hosp.setOrganisationType(hospDetails[2]);
-			hosp.setSubType(hospDetails[3]);
-			hosp.setSector(hospDetails[4]);
-			hosp.setOrganisationStatus(hospDetails[5]);
-			hosp.setIsPimsManaged(Boolean.getBoolean(hospDetails[6]));
-			hosp.setOrganisationName(hospDetails[7]);
-			hosp.setAddress1(hospDetails[8]);
-			hosp.setAddress2(hospDetails[9]);
-			hosp.setAddress3(hospDetails[10]);
-			hosp.setCity(hospDetails[11]);
-			hosp.setCounty(hospDetails[12]);
-			hosp.setPostcode(hospDetails[13]);
-//			hosp.setLatitude(Long.parseLong(hospDetails[14]));
-//			hosp.setLongitude(Long.parseLong(hospDetails[15]));
-			hosp.setParentODSCode(hospDetails[16]);
-			hosp.setParentName(hospDetails[17]);
-	//		hosp.setPhone(hospDetails[18]);
-	//		hosp.setEmail(hospDetails[19]);
-	//		hosp.setWebsite(hospDetails[20]);
-	//		hosp.setFax(hospDetails[21]);
-			Hospitals.add(hosp);		   
+			
+			if(hospDetails.length < 22) {
+				System.out.println("Adding : " + hospDetails[7] + " | " + hospDetails.length);
+								
+			} else {						
+				hosp.setOrganisationID(hospDetails[0]);
+				hosp.setOrganisationCode(hospDetails[1]);
+				hosp.setOrganisationType(hospDetails[2]);
+				hosp.setSubType(hospDetails[3]);
+				hosp.setSector(hospDetails[4]);
+				hosp.setOrganisationStatus(hospDetails[5]);
+				hosp.setIsPimsManaged(Boolean.getBoolean(hospDetails[6]));
+				hosp.setOrganisationName(hospDetails[7]);
+				hosp.setAddress1(hospDetails[8]);
+				hosp.setAddress2(hospDetails[9]);
+				hosp.setAddress3(hospDetails[10]);
+				hosp.setCity(hospDetails[11]);
+				hosp.setCounty(hospDetails[12]);
+				hosp.setPostcode(hospDetails[13]);
+//				hosp.setLatitude(Long.parseLong(hospDetails[14]));
+//				hosp.setLongitude(Long.parseLong(hospDetails[15]));
+				hosp.setParentODSCode(hospDetails[16]);
+				hosp.setParentName(hospDetails[17]);
+				hosp.setPhone(hospDetails[18]);
+				hosp.setEmail(hospDetails[19]);
+				hosp.setWebsite(hospDetails[20]);
+				hosp.setFax(hospDetails[21]);
+				Hospitals.add(hosp);		  	
+			}	 
 		}
 		scanner.close();
 		hospitalResp.save(Hospitals);
-
 	}
-
 }
