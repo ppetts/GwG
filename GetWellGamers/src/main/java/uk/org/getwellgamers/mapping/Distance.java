3e8938d27a4ec5ab11bf2d10bbcd3ea7f5e6aa6c
PETTSP2 <PETTSP2@II-LT043.tdwh.co.uk>
2018-05-17 07:24:09 +0100

    latest

--- GetWellGamers/src/main/java/uk/org/getwellgamers/mapping/Distance.java
+++ GetWellGamers/src/main/java/uk/org/getwellgamers/mapping/Distance.java
@@ -8,8 +8,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import uk.org.getwellgamers.person.Person;
 import uk.org.getwellgamers.staff.Staff;
 import uk.org.getwellgamers.staff.StaffService;
 
@@ -24,9 +30,13 @@ public class Distance implements CommandLineRunner{
 	private StaffService staffservice;
 	
 	private static final Logger log = LoggerFactory.getLogger(Distance.class);
+		
+	public Person getClosestPerson(String toPostcode) {
+		return getClosestPeople(1, toPostcode).first().person;
+	}
 	
 	
-	public TreeSet<DistanceWrapper> getClosestStaff(int howMany, String toPostcode) {
+	public TreeSet<DistanceWrapper> getClosestPeople(int howMany, String toPostcode) {
 		
 		//TODO Add URL Mapping?
 		TreeSet<DistanceWrapper> distanceSet = new TreeSet<DistanceWrapper>();
@@ -49,25 +59,6 @@ public class Distance implements CommandLineRunner{
 	
 
 	public double getDistance(String pc1, String pc2) {
-		return ThreadLocalRandom.current().nextDouble(0, 1000);
-	}
-	
-	
-
-	public void run(String... args) throws Exception {
-		
-		TreeSet<DistanceWrapper> results = getClosestStaff(2, "L40 3SF");
-		
-		DistanceWrapper firstDistWrapper = results.first();
-		
-	//	System.out.println("Closest:" + firstDistWrapper.person.getSurname() + "|distance:" + firstDistWrapper.distance);
-		
-		int count = 0;
-		for (DistanceWrapper dw : results) {
-			System.out.println(++count + ":" + dw.person.getSurname() + "|distance:" + dw.distance);
-		}
-		
-						
 		
 //		RestTemplate restTemplate = new RestTemplate();
 //
@@ -92,6 +83,52 @@ public class Distance implements CommandLineRunner{
 //		log.info(response.toString());
 //		log.info("==== RESTful API Response using Spring RESTTemplate END =======");
 //		//	    System.out.println("resp:" + response.getResult());
+		
+		
+		
+		return ThreadLocalRandom.current().nextDouble(0, 1000);
+	}
+	
+	
+
+	public void run(String... args) throws Exception {
+		
+		TreeSet<DistanceWrapper> results = getClosestPeople(2, "L40 3SF");
+		
+		DistanceWrapper firstDistWrapper = results.first();
+		
+	//	System.out.println("Closest:" + firstDistWrapper.person.getSurname() + "|distance:" + firstDistWrapper.distance);
+		
+		int count = 0;
+		for (DistanceWrapper dw : results) {
+			System.out.println(++count + ":" + dw.person.getSurname() + "|distance:" + dw.distance);
+		}
+		
+						
+		
+		RestTemplate restTemplate = new RestTemplate();
+
+	//	System.setProperty("proxyHost", "yourproxy.server.com"); 
+	//	System.setProperty("proxyPort", "8080");
+				
+		String fooResourceUrl = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=PR2%209HT&destinations=L403SF&mode=driving&language=GB&key=AIzaSyA03Gn1UI59XCq8ncK3sDixjI5XvM2mufs";
+		ResponseEntity<String> response = restTemplate.getForEntity(fooResourceUrl + "/1", String.class);
+		// assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode root = mapper.readTree(response.getBody());
+		System.out.println(root.toString());
+
+		JsonNode name = root.path("destination_addresses");
+		System.out.println(name);
+
+
+
+
+		//	    Response response = restTemplate.getForObject("https://maps.googleapis.com/maps/api/distancematrix/json?origins=PR2%209HT&destinations=L403SF&mode=driving&language=GB&key=AIzaSyA03Gn1UI59XCq8ncK3sDixjI5XvM2mufs",Response.class);
+		log.info("==== RESTful API Response using Spring RESTTemplate START =======");
+		log.info(response.toString());
+		log.info("==== RESTful API Response using Spring RESTTemplate END =======");
+		//	    System.out.println("resp:" + response.getResult());
 	}
 }
 
---
+++ GetWellGamers/src/main/java/uk/org/getwellgamers/mapping/DistanceService.java
@@ -0,0 +1,5 @@
+package uk.org.getwellgamers.mapping;
+
+public class DistanceService {
+
+}
