c24cf5a90cc446907e351e552151554150c850ce
PETTSP2 <PETTSP2@II-LT043.tdwh.co.uk>
2018-06-14 06:42:34 +0100

    thymeleaf

--- GetWellGamers/src/main/java/uk/org/getwellgamers/GwGController.java
+++ GetWellGamers/src/main/java/uk/org/getwellgamers/GwGController.java
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("TODO")
+@RequestMapping("/help")
 class GwGController{
 
 	@RequestMapping(method = RequestMethod.GET)
 	  public String root(Locale locale, ModelMap model) {
 		   model.addAttribute("content", "GwGIndex");     
 		  // return "index";
-		   return   "redirect:index.html";
+		   return   "redirect:resources/static/index.html";
 		 
 	}
 }
